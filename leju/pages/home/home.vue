364afcafbd1dea46d771b63255f9267251a1a8c1
想走过亚洲的熊 <1693889638@qq.com>
2022-09-03 17:09:58 +0800

    项目初始化完成

---
+++ leju/pages/home/home.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>home</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	onLoad() {
+		uni.request({
+			url: "http://leju.bufan.cloud/lejuclient/home/bannerAds",
+			method: "GET",
+			timeout: 5000,
+			success(res) {
+				console.log(res.data);
+			},
+		});
+	},
+};
+</script>
+
+<style lang="scss"></style>
